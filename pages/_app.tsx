8ac3232eebd648d4efd9d6951e3fb5babc6029e1
Anakin Yuen <anakinyuen1024@gmail.com>
2020-08-10 01:45:52 +0800

    :shirt: refactor: wrap component with Layout and providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
 import { AppProps } from 'next/app';
+import React from 'react';
+import './fonts.css';
 import './global.scss';
+import './unify.scss';
+import Layout from '../components/Layout';
+import ChatProvider from '../providers/chat';
+import TabProvider from '../providers/tab';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <TabProvider>
+      <ChatProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChatProvider>
+    </TabProvider>
+  );
 };
 
 export default MyApp;

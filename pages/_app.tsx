b2e80790cea56612b4f6fbf09c8e1524763f1949
Anakin Yuen <anakinyuen1024@gmail.com>
2020-08-10 18:12:35 +0800

    :heavy_plus_sign: update: add assets for PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 import './fonts.css';
 import './global.scss';
@@ -9,13 +10,39 @@ import TabProvider from '../providers/tab';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <TabProvider>
-      <ChatProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChatProvider>
-    </TabProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="keywords" content="Anakin Yuen" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/icons/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/icons/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/icons/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-icon-180x180.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icons/android-icon-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <meta name="theme-color" content="#1E1E1E" />
+      </Head>
+      <TabProvider>
+        <ChatProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChatProvider>
+      </TabProvider>
+    </>
   );
 };
 

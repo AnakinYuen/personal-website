e39293d821920a38fe7b05fa11f7df394aa4c873
Anakin Yuen <anakinyuen1024@gmail.com>
2020-08-10 18:15:29 +0800

    :star: feature: define Workbox Strategies

---
+++ sw.js
@@ -0,0 +1,138 @@
+import { ExpirationPlugin } from 'workbox-expiration';
+import { precacheAndRoute } from 'workbox-precaching';
+import { RangeRequestsPlugin } from 'workbox-range-requests';
+import { registerRoute } from 'workbox-routing';
+import { CacheFirst, NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute(
+  /\/(about|contact|home|skill|work)?$/,
+  new NetworkFirst({
+    cacheName: 'start-url',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\.mp4$/,
+  new StaleWhileRevalidate({
+    cacheName: 'video',
+    plugins: [
+      new RangeRequestsPlugin(),
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+  new CacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-font-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-image-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\.(?:js)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-js-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\.(?:css|less)$/i,
+  new StaleWhileRevalidate({
+    cacheName: 'static-style-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\.(?:json|xml|csv)$/i,
+  new NetworkFirst({
+    cacheName: 'static-data-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+  }),
+);
+
+registerRoute(
+  /\/api\/.*$/i,
+  new NetworkFirst({
+    cacheName: 'apis',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+    networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+  }),
+);
+
+registerRoute(
+  /.*/i,
+  new NetworkFirst({
+    cacheName: 'others',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
+      }),
+    ],
+    networkTimeoutSeconds: 10,
+  }),
+);
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open('start-url')
+      .then((cache) => cache.addAll(['/', '/about', '/contact', '/home', '/skill', '/work'])),
+  );
+});

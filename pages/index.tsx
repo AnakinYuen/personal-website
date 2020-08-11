f911293ae14491372db4ce3005b907077d46621b
Anakin Yuen <anakinyuen1024@gmail.com>
2020-08-11 12:14:53 +0800

    :on: update: redirect to home page on first visit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,19 @@
+import { useRouter } from 'next/router';
+import React from 'react';
 import IndexPage from '../components/pages/IndexPage';
-export default IndexPage;
+import { addTab, TabContext } from '../providers/tab';
+
+export default (): ReturnType<React.FC> => {
+  const router = useRouter();
+  const { dispatch } = React.useContext(TabContext);
+
+  React.useEffect(() => {
+    const noFirstVisit = sessionStorage.getItem('noFirstVisit');
+    if (!noFirstVisit) {
+      sessionStorage.setItem('noFirstVisit', 'true');
+      router.push('/home');
+      dispatch(addTab('/home'));
+    }
+  }, []);
+  return <IndexPage />;
+};

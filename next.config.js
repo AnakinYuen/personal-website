ebb2f4a020af1c62296bb88a1bedd388aea4a7cd
Anakin Yuen <anakinyuen1024@gmail.com>
2020-08-11 12:11:31 +0800

    :on: update: enable PWA support

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    swSrc: './sw.js',
+  },
+});
